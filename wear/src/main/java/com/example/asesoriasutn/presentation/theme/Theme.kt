d6e7d57b47685053ef46defb682a13467d89812e
YorelGutierrez <fetillorel@gmail.com>
2026-07-19 11:57:15 -0700

    cargainicial

---
+++ wear/src/main/java/com/example/asesoriasutn/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.asesoriasutn.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun AsesoriasUTNTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
