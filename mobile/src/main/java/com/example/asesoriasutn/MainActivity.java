bf2fac0cf5d749ef60eaa3ed26a17293b50ab45e
Vanessa1728 <tic-310073@utnay.edu.mx>
2026-07-21 16:23:36 -0700

    login

--- mobile/src/main/java/com/example/asesoriasutn/MainActivity.java
+++ mobile/src/main/java/com/example/asesoriasutn/MainActivity.java
@@ -5,31 +5,41 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etCorreo, etPassword;
+    private EditText txtCorreo, txtPassword;
     private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // Vinculación segura de los elementos del XML
-        etCorreo = findViewById(R.id.etCorreo);
-        etPassword = findViewById(R.id.etPassword);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        txtCorreo = findViewById(R.id.txtCorreo);
+        txtPassword = findViewById(R.id.txtPassword);
         btnLogin = findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String correo = etCorreo.getText().toString().trim();
-                String password = etPassword.getText().toString().trim();
+                String correo = txtCorreo.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
 
                 if (correo.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Por favor llena todos los campos", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Por favor completa todos los campos", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(MainActivity.this, "Conectando al servidor...", Toast.LENGTH_SHORT).show();
                 }
